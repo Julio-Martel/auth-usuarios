8dfbe1d975e9e174d92255eefdff5fc98aecd919
Julio-Martel <julioramdom123@gmail.com>
2026-03-22 11:46:51 -0300

    feat: agrega base de datos en usuarios.js

---
+++ database/usuarios.js
@@ -0,0 +1,7 @@
+const usuarios = [
+    {id: 1, nombre: 'Julio', apellido: 'Martel', usuario: 'Julio4561', password: 'Climax4561'},
+    {id: 2, nombre: 'Sakura', apellido: 'Yoshida', usuario: 'saku1024', password: 'rsa20245'},
+    {id: 3, nombre: 'Grace', apellido: 'Smith', usuario: 'kia123', password: 'jassxw45167'}  
+];
+
+module.exports = usuarios;
